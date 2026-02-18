7600b5ec85af977f454c72204e2d170d1137678a
slomalx <leemoore88@yandex.ru>
2026-02-18 13:12:54 +0300

    логика без адаптива

--- script.js
+++ script.js
@@ -1,1 +1,95 @@
+const textarea = document.getElementById('textarea');
+const lite = document.getElementById('lite_button');
+const medium = document.getElementById('medium_button');
+const goida = document.getElementById('goida_button');
+const baza = document.getElementById('baza_button');
 
+
+let selectedMode = null;
+
+
+function setActiveMode(mode) {
+
+    lite.classList.remove('active');
+    medium.classList.remove('active');
+    goida.classList.remove('active');
+
+
+    if (mode === 'lite') {
+        lite.classList.add('active');
+    } else if (mode === 'medium') {
+        medium.classList.add('active');
+    } else if (mode === 'goida') {
+        goida.classList.add('active'); 
+    }
+}
+
+
+lite.addEventListener('click', () => {
+    selectedMode = 'lite';
+    setActiveMode('lite');
+    console.log('Выбран режим: Lite');
+});
+
+medium.addEventListener('click', () => {
+    selectedMode = 'medium';
+    setActiveMode('medium');
+    console.log('Выбран режим: Medium');
+});
+
+goida.addEventListener('click', () => {
+    selectedMode = 'goida';
+    setActiveMode('goida');
+    console.log('Выбран режим: Goida');
+});
+
+
+function replaceLite(text) {
+    return text
+        .replace(/з/g, 'z')
+        .replace(/З/g, 'Z');
+}
+
+function replaceMedium(text) {
+    return text
+        .replace(/з/g, 'z')
+        .replace(/З/g, 'Z')
+        .replace(/в/g, 'v')
+        .replace(/В/g, 'V');
+}
+
+
+function replaceGoida(text) {
+    return text
+        .replace(/з/g, 'Z')
+        .replace(/З/g, 'Z')
+        .replace(/в/g, 'V')
+        .replace(/В/g, 'V')
+        .replace(/о/g, 'O')
+        .replace(/с/g, 'Z')
+        .replace(/С/g, 'Z');
+}
+
+
+baza.addEventListener('click', function() {
+
+    const originalText = textarea.value;
+    let bazedText = originalText;
+    
+    if (!selectedMode) {
+        alert('Выберите баZOVость');
+        return;
+    }
+    
+    if (selectedMode === 'lite') {
+        bazedText = replaceLite(originalText);
+    } else if (selectedMode === 'medium') {
+        bazedText = replaceMedium(originalText);
+    } else if (selectedMode === 'goida') {
+        bazedText = replaceGoida(originalText);
+    }
+
+    textarea.value = bazedText;
+    
+
+});
